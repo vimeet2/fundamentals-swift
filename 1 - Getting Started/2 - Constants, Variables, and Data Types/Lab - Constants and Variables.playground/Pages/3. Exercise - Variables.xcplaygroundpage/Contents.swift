83cf1a28a657544502dd07642acc033278cdc7e4
Vimeet <102480142+vimeet2@users.noreply.github.com>
2024-04-17 16:15:07 +0530

    Add files via upload

---
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+## Exercise - Variables
+ 
+ Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
+ */
+var schooling = 5
+print(schooling)
+//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+schooling = 7
+print(schooling)
+/*:
+[Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
+ */
